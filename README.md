4723fc4b41e0b3991988e61646d55cf3fd4d444b
Drahsid <Drahsid@gmail.com>
2021-12-07 02:27:33 -0500

    Many bugfixes and improvements to UX

--- README.md
+++ README.md
@@ -4,29 +4,31 @@ A shell script that installs DarkflameServer locally
 ## Before you use
 Make sure that nothing is binding the ports for mysql.
 
-I highly suggest making a sql user instead of using root. You can do this by running these commands:
+I highly suggest making a sql user instead of using root. If you already have mysql installed, you can simply run:
 ```sql
 create user 'USERNAME'@'localhost' identified by 'PASSWORD';
 grant all privilages on *.* to 'USERNAME'@'localhost';
 ```
 
-This user will be the same user that is the "sqlUsername" below.
+otherwise, you will be promted to create the user during the mysql installation in the script.
 
 ## Usage
 simply download [install_darkflame.sh](install_darkflame.sh) from this repository and run it with super-user privilages.
-
 ```shell
 wget https://raw.githubusercontent.com/Drahsid/DarkflameServerInstallScript/master/install_darkflame.sh
-
 sudo chmod +x ./install_darkflame.sh
-
-sudo ./install_darkflame.sh /path/to/res/folder
+sudo ./install_darkflame.sh
 ```
 
-Note that you can provide additional arguments for further control of the installation process. If you don't provide these arguments, the script will ask you for them.
+or as one command
+```shell
+wget https://raw.githubusercontent.com/Drahsid/DarkflameServerInstallScript/master/install_darkflame.sh && sudo chmod +x ./install_darkflame.sh && sudo ./install_darkflame.sh
+```
+
+Note that you can provide additional arguments for further automation of the installation process. If you don't provide these, the script will ask you for what it needs.
 
 ```
-sudo ./install_darkflame.sh /path/to/res/folder sqlUsername databaseName path/to/install/server
+sudo ./install_darkflame.sh /path/to/res/folder sqlUsername databaseName path/to/download/server path/to/build/server
 ```
 
 ### Additional Contributors
--- install_darkflame.sh
+++ install_darkflame.sh
@@ -1,97 +1,104 @@
 #!/bin/bash
 
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 PURPLE='\033[0;35m'
 NOCOLOR='\033[0m'
 
 if [[ $EUID -ne 0 ]]; then
-    echo -e "${RED}This script must be run as root!${NOCOLOR}"
+    echo -e "${RED}this script must be run as root!${NOCOLOR}"
     exit 1
 fi
 
 # $1 resource dir $2 sql user $3 database name $4 server dir $5 build dir
 
 if [ -z $1 ]; then
-    read -p 'please input the path to the res folder: ' -n 1 -r
-    resDir=$(realpath $REPLY)
-    if [ -z $resDir ]; then
+    read -p "please input the path to the res folder$(echo \n)this directory should be the the folder titles res in the client: " -r
+    resDir=${REPLY}
+    if [ -z ${resDir} ]; then
         echo -e "${RED}ERROR! YOU MUST PROVIDE THE RESOURCE DIRECTORY!${NOCOLOR}"
         exit 1
     fi
+else
+    resDir=$1
 fi
-resDir=$(realpath $1)
-echo -e "${PURPLE}using resource dir $resDir${NOCOLOR}"
+resDir=$(realpath ${resDir})
+echo -e "${PURPLE}using resource dir ${GREEN}${resDir}${NOCOLOR}\n"
 
 if [ -z $2 ]; then
-    read -p 'sql user not provided, would you like to provide one? if no, default will be root. make sure this user exists! [y/n] ' -n 1 -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        read -p 'input sql username now: ' -n 1 -r
-        sqlUser=$REPLY
+    read -p "sql user not provided, would you like to provide one? if no, default will be $(echo -e ${GREEN}root${NOCOLOR}). make sure this user exists! [y/n] " -n 1 -r
+    if [[ ! ${REPLY} =~ ^[Nn]$ ]]; then
+        echo \n
+        read -p 'input sql username now: ' -r
+        sqlUser=${REPLY}
     else
-        echo -e "${RED}no sql user defined, using root${NOCOLOR}"
+        echo -e "\n${RED}no sql user defined, using ${GREEN}root${NOCOLOR}"
         sqlUser=root
     fi
 else
     sqlUser=$2
 fi
-echo -e "${PURPLE}using sql user $2${NOCOLOR}"
+echo -e "${PURPLE}using sql user ${GREEN}${sqlUser}${NOCOLOR}\n"
 
 if [ -z $3 ]; then
-    read -p 'database name not provided, would you like to provdie one? if no, default will be dlu [y/n] ' -n 1 -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        read -p 'input database name now: ' -n 1 -r
-        databaseName=$REPLY
+    read -p "database name not provided, would you like to provdie one? if no, default will be $(echo -e ${GREEN}dlu${NOCOLOR}) [y/n] " -n 1 -r
+    if [[ ! ${REPLY} =~ ^[Nn]$ ]]; then
+        read -p '\ninput database name now: ' -r
+        databaseName=${REPLY}
     else
-        echo -e "${RED}no database name defined, using dlu${NOCOLOR}"
+        echo -e "\n${RED}no database name defined, using ${GREEN}dlu${NOCOLOR}"
         databaseName=dlu
     fi
 else
     databaseName=$3
 fi
-echo -e "${PURPLE}using database name $databaseName${NOCOLOR}"
+echo -e "${PURPLE}using database name ${GREEN}${databaseName}${NOCOLOR}\n"
 
 if [ -z $4 ]; then
-    read -p 'server files dir not provided, would you like to provide one? if no, default will be . [y/n] ' -n 1 -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        read -p 'input server files dir: ' -n 1 -r
-        serverDir = $(realpath $REPLY)
+    read -p "server files dir not provided, would you like to provide one? if no, default will be $(echo -e ${GREEN}.${NOCOLOR})$(echo \n)this directory is the one which contains the files that are used to build the server. [y/n] " -n 1 -r
+    if [[ ! ${REPLY} =~ ^[Nn]$ ]]; then
+        echo \n
+        read -p 'input server files dir: ' -r
+        mkdir -p ${REPLY} # premake dir
+        serverDir=${REPLY}
     else
-        serverDir=$(realpath .)
-        echo -e "${RED}no server dir defined, using ${serverDir}${NOCOLOR}"
+        serverDir=.
+        echo -e "\n${RED}no server dir defined, using ${GREEN}${serverDir}${NOCOLOR}"
     fi
 else
-    serverDir=$(realpath $4)
+    serverDir=$4
 fi
-serverDir=$(realpath $serverDir)
-echo -e "${PURPLE}using server dir $serverDir${NOCOLOR}"
+serverDir=$(realpath ${serverDir})
+echo -e "${PURPLE}using server dir ${GREEN}${serverDir}${NOCOLOR}\n"
 
 usingBuildDir=0
 if [ -z $5 ]; then
-    read -p 'if you have already built the server files, put the path to them here (otherwise leave this blank): '
-    if [ -z $REPLY ]; then
-        buildDir=$serverDir/build
+    read -p "if you have already built the server files, put the path to them here $(echo -e ${GREEN}\(otherwise leave this blank\)${NOCOLOR}): " -r
+
+    if [ -z ${REPLY} ]; then
+        buildDir=${serverDir}/build
     else
-        buildDir=$REPLY
+        buildDir=${REPLY}
         usingBuildDir=1
     fi
 else
     buildDir=$5
     usingBuildDir=1
 fi
-echo -e "${PURPLE}using build dir $buildDir${NOCOLOR}"
+echo -e "${PURPLE}using build dir ${GREEN}${buildDir}${NOCOLOR}\n"
 
-echo -e "${PURPLE}installing required packages...${NOCOLOR}"
+echo -e "${PURPLE}installing required packages${NOCOLOR}\n"
 apt update
 apt install gcc cmake build-essential zlib1g-dev python3 python3-pip unzip sqlite3
 
 read -p 'do you want to install mysql and mariadb? (you can choose N if you have already installed these!) [y/n] ' -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+if [[ ! ${REPLY} =~ ^[Nn]$ ]]; then
     # potentially problematic
     if [ apt install mariadb-server ]; then
         echo -e "${NOCOLOR}"
     else
-        read -p 'installing mariadb-server failed, would you like CLEAN install them? (may be dangerous if you already have either set up and in-use.) If you select no, we will install mariadb-server-10.3 instead. [y/n] ' -n 1 -r
-        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        read -p "installing mariadb-server failed, would you like CLEAN install them?$(echo \n)doing so may be dangerous if you already have either mysql or mariadb set up and in-use, this purges the relevant packages.\nif you select no, we will install mariadb-server-10.3 instead [y/n] " -n 1 -r
+        if [[ ! ${REPLY} =~ ^[Nn]$ ]]; then
             apt clean
             apt purge 'mysql*'
             apt purge 'mariadb*'
@@ -100,7 +107,7 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
             if [ apt install mariadb-server ]; then
                 echo -e "${NOCOLOR}"
             else
-                echo -e "${RED}failed! falling back to mariadb-server-10.3${NOCOLOR}"
+                echo -e "${RED}warning: failed! falling back to mariadb-server-10.3 (this should be OK!)${NOCOLOR}"
                 apt install mariadb-server-10.3
             fi
         else
@@ -108,64 +115,92 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         fi
     fi
 
-    echo -e "${PURPLE}setting up mysql...${NOCOLOR}"
+    echo -e "${PURPLE}setting up mysql${NOCOLOR}"
     service mysql start
     mysql_secure_installation
 fi
+echo \n
 
-if [ $usingBuildDir -eq "0" ]; then
-    echo -e "${PURPLE}cloning repository...${NOCOLOR}"
-    git clone https://github.com/DarkflameUniverse/DarkflameServer.git --recursive $serverDir
+if [ ${usingBuildDir} -eq "0" ]; then
+    echo -e "${PURPLE}cloning DarkflameServer repository${NOCOLOR}"
+    git clone https://github.com/DarkflameUniverse/DarkflameServer.git --recursive ${serverDir}
 else
-    echo -e "${PURPLE}using server build files $buildDir${NOCOLOR}"
+    echo -e "${PURPLE}using server build files ${GREEN}${buildDir}${NOCOLOR}"
 fi
 
-cd $serverDir
-mkdir -p $buildDir
-cd $buildDir
+cd ${serverDir}
+mkdir -p ${buildDir}
+cd ${buildDir}
 
-echo -e "${PURPLE}building server...${NOCOLOR}"
+echo -e "${PURPLE}building server${NOCOLOR}\n"
 cmake ..  && make -j4
 
-echo -e "${PURPLE}setting up mysql database${NOCOLOR}"
-mysql -u $sqlUser -e "drop database $databaseName";
-mysql -u $sqlUser -e "create database $databaseName";
-mariadb $databaseName < $serverDir/migrations/dlu/0_initial.sql;
+echo -e "${PURPLE}setting up mysql database${NOCOLOR}\n"
 
-echo -e "${PURPLE}setting up resource folder${NOCOLOR}"
-rm -rf $buildDir/res
-ln -s $resDir $buildDir/
+if [ "${sqlUser}" != "root" ]; then
+    read -p "do you want to create the sqlUser ${sqlUser} now? (select N if you have already created the user!) [y/n] " -n 1 -r
+    if [[ ! ${REPLY} =~ ^[Nn]$ ]]; then
+        echo \n
+        read -p "input desired password for sql user ${sqlUser}: " -r
+        sqlUserPW=${REPLY}
+        mysql -e "create user '${sqlUser}'@'localhost' identified by '${sqlUserPW}'";
+        mysql -e "grant all privileges on *.* to '${sqlUser}'@'localhost'";
+    fi
+else
+    echo -e ${RED}sqlUser is root, no need to create${NOCOLOR}
+fi
 
-echo -e "${PURPLE}setting up navmeshes${NOCOLOR}"
-mkdir $buildDir/res/maps/navmeshes
-unzip $serverDir/resources/navmeshes.zip -d $buildDir/res/maps
+mysql -e "drop database ${databaseName}"; # hopefully noone loses any data because of this
+mysql -e "create database ${databaseName}";
+mariadb ${databaseName} < ${serverDir}/migrations/dlu/0_initial.sql;
 
-echo -e "${PURPLE}setting up locale${NOCOLOR}"
-rm -rf $buildDir/locale
-ln -s $buildDir/res/locale $buildDir/
+echo -e "${PURPLE}setting up resource folder${NOCOLOR}\n"
+rm -rf ${buildDir}/res
+ln -s ${resDir} ${buildDir}/
 
-echo -e "${PURPLE}setting up CDServer.sqlite${NOCOLOR}"
-git clone https://github.com/lcdr/utils.git $serverDir/lcdrutils
-python3 $serverDir/lcdrutils/utils/fdb_to_sqlite.py $buildDir/res/cdclient.fdb --sqlite_path $buildDir/res/CDServer.sqlite
+echo -e "${PURPLE}setting up navmeshes${NOCOLOR}\n"
+mkdir ${buildDir}/res/maps/navmeshes
+unzip ${serverDir}/resources/navmeshes.zip -d ${buildDir}/res/maps
 
-sqlite3 $buildDir/res/CDServer.sqlite ".read $serverDir/migrations/cdserver/0_nt_footrace.sql"
-sqlite3 $buildDir/res/CDServer.sqlite ".read $serverDir/migrations/cdserver/1_fix_overbuild_mission.sql"
-sqlite3 $buildDir/res/CDServer.sqlite ".read $serverDir/migrations/cdserver/2_script_component.sql"
+echo -e "${PURPLE}setting up locale${NOCOLOR}\n"
+rm -rf ${buildDir}/locale
+ln -s ${resDir}/../locale ${buildDir}/
 
-echo -e "${PURPLE}setting up configs (If your sql user has a password, make sure to edit the files and input the password after \`mysql_password=\`)${NOCOLOR}"
-sed -i "s/mysql_host=/mysql_host=localhost/g" $buildDir/authconfig.ini
-sed -i "s/mysql_database=/mysql_database=$databaseName/g" $buildDir/authconfig.ini
-sed -i "s/mysql_username=/mysql_username=$sqlUser/g" $buildDir/authconfig.ini
+echo -e "${PURPLE}setting up CDServer.sqlite${NOCOLOR}\n"
+git clone https://github.com/lcdr/utils.git ${serverDir}/lcdrutils
+python3 ${serverDir}/lcdrutils/utils/fdb_to_sqlite.py ${buildDir}/res/cdclient.fdb --sqlite_path ${buildDir}/res/CDServer.sqlite
 
-sed -i "s/mysql_host=/mysql_host=localhost/g" $buildDir/chatconfig.ini
-sed -i "s/mysql_database=/mysql_database=$databaseName/g" $buildDir/chatconfig.ini
-sed -i "s/mysql_username=/mysql_username=$sqlUser/g" $buildDir/chatconfig.ini
+sqlite3 ${buildDir}/res/CDServer.sqlite ".read ${serverDir}/migrations/cdserver/0_nt_footrace.sql"
+sqlite3 ${buildDir}/res/CDServer.sqlite ".read ${serverDir}/migrations/cdserver/1_fix_overbuild_mission.sql"
+sqlite3 ${buildDir}/res/CDServer.sqlite ".read ${serverDir}/migrations/cdserver/2_script_component.sql"
 
-sed -i "s/mysql_host=/mysql_host=localhost/g" $buildDir/masterconfig.ini
-sed -i "s/mysql_database=/mysql_database=$databaseName/g" $buildDir/masterconfig.ini
-sed -i "s/mysql_username=/mysql_username=$sqlUser/g" $buildDir/masterconfig.ini
+echo -e "${PURPLE}setting up configs (If your sql user has a password, make sure to edit the files and input the password after \`mysql_password=\`)${NOCOLOR}\n"
+sed -i "s/mysql_host=/mysql_host=localhost/g" ${buildDir}/authconfig.ini
+sed -i "s/mysql_database=/mysql_database=${databaseName}/g" ${buildDir}/authconfig.ini
+sed -i "s/mysql_username=/mysql_username=${sqlUser}/g" ${buildDir}/authconfig.ini
 
-sed -i "s/mysql_host=/mysql_host=localhost/g" $buildDir/worldconfig.ini
-sed -i "s/mysql_database=/mysql_database=$databaseName/g" $buildDir/worldconfig.ini
-sed -i "s/mysql_username=/mysql_username=$sqlUser/g" $buildDir/worldconfig.ini
+sed -i "s/mysql_host=/mysql_host=localhost/g" ${buildDir}/chatconfig.ini
+sed -i "s/mysql_database=/mysql_database=${databaseName}/g" ${buildDir}/chatconfig.ini
+sed -i "s/mysql_username=/mysql_username=${sqlUser}/g" ${buildDir}/chatconfig.ini
 
+sed -i "s/mysql_host=/mysql_host=localhost/g" ${buildDir}/masterconfig.ini
+sed -i "s/mysql_database=/mysql_database=${databaseName}/g" ${buildDir}/masterconfig.ini
+sed -i "s/mysql_username=/mysql_username=${sqlUser}/g" ${buildDir}/masterconfig.ini
+
+sed -i "s/mysql_host=/mysql_host=localhost/g" ${buildDir}/worldconfig.ini
+sed -i "s/mysql_database=/mysql_database=${databaseName}/g" ${buildDir}/worldconfig.ini
+sed -i "s/mysql_username=/mysql_username=${sqlUser}/g" ${buildDir}/worldconfig.ini
+
+if [ -z ${sqlUserPW} ]; then
+    echo -e "${RED}Since you did not create the sql user using this script, be sure to fill out its password in authconfig.ini chatconfig.ini masterconfig.ini worldconfig.ini!${NOCOLOR}"
+else
+    sed -i "s/mysql_password=/mysql_password=${sqlUserPW}/g" ${buildDir}/authconfig.ini
+    sed -i "s/mysql_password=/mysql_password=${sqlUserPW}/g" ${buildDir}/chatconfig.ini
+    sed -i "s/mysql_password=/mysql_password=${sqlUserPW}/g" ${buildDir}/masterconfig.ini
+    sed -i "s/mysql_password=/mysql_password=${sqlUserPW}/g" ${buildDir}/worldconfig.ini
+fi
+
+echo -e "${PURPLE}Done! Should be OK!${NOCOLOR}\n"
+echo -e "${PURPLE}You can make the admin account by navigating to ${buildDir} and running \`./MasterServer -a\`, and you can start the server by running \`./MasterServer\` in that same directory${NOCOLOR}\n"
+echo -e "${PURPLE}If you want external connections, make sure to change the \`external_ip\` property in the ini files to be your front-facing domain or ip!${NOCOLOR}\n"
+echo -e "${PURPLE}Consider chowning and chmoding the files in ${buildDir} so that you don't need to use sudo on everything${NOCOLOR}"
